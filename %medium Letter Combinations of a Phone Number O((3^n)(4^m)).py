a71147fb89f87cb9240e9415e304e36e944432ed
noahk0 <noah.kim@mail.utoronto.ca>
2024-08-24 14:30:45 +0900

    Update %medium Letter Combinations of a Phone Number O((3^n)(4^m)).py

--- %medium Letter Combinations of a Phone Number O((3^n)(4^m)).py
+++ %medium Letter Combinations of a Phone Number O((3^n)(4^m)).py
@@ -1,7 +1,4 @@
 def letterCombinations(self, digits: str) -> List[str]:
-    if not digits:
-        return []
-
     combination, button = [''], {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
     for digit in digits:
@@ -12,4 +9,4 @@ def letterCombinations(self, digits: str) -> List[str]:
 
         combination = new
 
-    return combination
+    return combination if digits else []

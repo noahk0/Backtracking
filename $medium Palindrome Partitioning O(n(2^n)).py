b47c739affff83dcb4d31b4526b4d8e6fd355f25
noahk0 <noah.kim@mail.utoronto.ca>
2024-08-24 14:28:14 +0900

    Update $medium Palindrome Partitioning O(n(2^n)).py

--- $medium Palindrome Partitioning O(n(2^n)).py
+++ $medium Palindrome Partitioning O(n(2^n)).py
@@ -1,9 +1,2 @@
 def partition(self, s: str) -> List[List[str]]:
-    palindrome = []
-
-    for i in range(1, len(s) + 1):
-        if s[:i] == s[:i][::-1]:
-            for rest in self.partition(s[i:]):
-                palindrome.append([s[:i]] + rest)
-
-    return palindrome if s else [[]]
+    return [[s[:i]] + rest for i in range(1, len(s) + 1) if s[:i] == s[:i][::-1] for rest in self.partition(s[i:])] if s else [[]]
